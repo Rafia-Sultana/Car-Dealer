340676a7441dfaefbb0a2226a7083adfef5c88fe
Rafia Sultana <rafiasultana0097@gmail.com>
2022-05-08 22:54:19 +0600

    updated

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -13,7 +13,7 @@ const Blog = () => {
                 <p>Answer: NodeJS is a JavaScript runtime environment. It's actually helps JavaScript to run outside of server. It's used in server side development.MongoDB is NoSQL database which  stores data and also document oriented. It represents data as of JSON documents.Simply,we can say  MongoDB is a database where we can store data and NodeJS helps us to  connect our client site to database by it's server site.</p>
             </div>
             <div className="shadow rounded">
-                <h5 className="card-header">Differences between sql and nosql databases.</h5>
+                <h5 className="card-header">Differences between sql and nosql databases</h5>
                 <p className="card-body">Answer: 	SQL databases known as table-based database because of displaying data in form of tables whereas NoSQL databases display data as collection of key-value pair, documents, graph databases or wide-column stores.In SQL,Databases are categorized as Relational Database Management System .On the other hand,NoSQL databases are categorized as Non-relational database system.NoSQL databases have dynamic schema.</p>
             </div>
 
